74c8e01bbaf18a2e73ae9272bd6162cd43d7565e
SEENUSHARMA <yashshar.2323@gmail.com>
2023-03-31 10:32:09 +0530

    adding post section to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,30 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import memories from './image/memories.png'
 
 const App = () => {
     return(
-        <div>
-            <h1>
-                App
-            </h1>
-        </div>
+        <Container maxWidth="lg" >
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Memories</Typography>
+                <img src={memories} alt="memories" height="200" width="150" />
+            </AppBar>
+            <Grow in> 
+                <Container>
+                    <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}> 
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     )
 }
 

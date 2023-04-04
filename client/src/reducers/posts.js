b5e20dbd11991b164d8aaddb6bd96062c5eed40a
yashsharma127 <yashshar.2323@gmail.com>
2023-04-04 17:12:00 +0530

    Making case for like and update same because we need same return value for both

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -5,11 +5,10 @@ export default (posts = [], action) => {
     case FETCH_ALL:
       return action.payload;
     case LIKE:
+    case UPDATE:
       return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
     case CREATE:
       return [...posts, action.payload];
-    case UPDATE:
-      return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
     case DELETE:
       return posts.filter((post) => post._id !== action.payload);
     default:
